3f7bb39553183ed75cd5577717320f85d648a104
mkartaev <mart92k@hotmail.com>
2018-05-09 23:42:17 +0200

    RemoveUneccesaryLoop

--- bot/bot.cpp
+++ bot/bot.cpp
@@ -22,16 +22,13 @@ void Bot::OnGameStart() {
             bot.Debug()->DebugSphereOut(Point3D(p.x + 0.5, p.y - 0.5, startLocation_.z), 0.5, Colors::Red);
         }
     }
-    for (int depth = 1; depth < 2; ++depth) {
-        for (int i = 0; i < size; ++i) {
-            if (diff[i] == depth) {
-                for (int j = 0; j < size; ++j) {
-                    Point2D p = GetMapCoordinate(j);
-                    if (Distance2D(GetMapCoordinate(i),p) == 2 && game_info_.placement_grid.data[j] != 0) {
-                        bot.Debug()->DebugSphereOut(Point3D(p.x + 0.5, p.y - 0.5, startLocation_.z), 0.5, Colors::Blue);
-                        diff[j] = depth + 1;
-                    }
-                }
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            Point2D p = GetMapCoordinate(j);
+            if (ManhattanDistance(GetMapCoordinate(i),p) <= 3 && game_info_.placement_grid.data[j] != 0) {
+                bot.Debug()->DebugSphereOut(Point3D(p.x + 0.5, p.y - 0.5, startLocation_.z), 0.5, Colors::Blue);
+                diff[j] = 2;
             }
         }
     }
--- bot/main.cpp
+++ bot/main.cpp
@@ -8,6 +8,15 @@
 using namespace sc2;
 using namespace BOT;
 
+const char* kMapBelShirVestigeLE = "Ladder/(2)Bel'ShirVestigeLE (Void).SC2Map";
+const char* AcidPlantLE = "Ladder/AcidPlantLE.SC2Map";
+const char* AbiogenesisLE = "Ladder/AbiogenesisLE.SC2Map";
+const char* BlackwaterLE = "Ladder/BackwaterLE.SC2Map";
+const char* CatalystLE = "Ladder/CatalystLE.SC2Map";
+const char* NeonVioletSquareLE = "Ladder/NeonVioletSquareLE.SC2Map";
+
+
+
 int main(int argc, char* argv[]) {
     Coordinator coordinator;
     if (!coordinator.LoadSettings(argc, argv)) {
@@ -27,7 +36,7 @@ int main(int argc, char* argv[]) {
     bool do_break = false;
 
     for (; !do_break;) {
-        coordinator.StartGame(kMapBelShirVestigeLE);
+        coordinator.StartGame(CatalystLE);
 
         while (coordinator.Update() && !do_break) {
             if (PollKeyPress()) {
