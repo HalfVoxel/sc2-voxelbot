bf10fae5c3720701f82986091fee301be1bfb710
Aron Granberg <aron.granberg@gmail.com>
2018-12-21 02:33:43 +0100

    Add missing build optimizer main file

---
+++ bot/build_optimizer/main.cpp
@@ -0,0 +1,11 @@
+#include "../utilities/mappings.h"
+#include "../BuildOptimizerGenetic.h"
+
+int main() {
+    initMappings();
+    // BuildOptimizer optimizer;
+    // optimizer.init();
+    // unitTestBuildOptimizer(optimizer);
+    unitTestBuildOptimizer();
+    return 0;
+}
