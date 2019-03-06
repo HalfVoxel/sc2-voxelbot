968cfc77a4c7f8441b43780ec41c23c599c94c4e
Aron Granberg <aron.granberg@gmail.com>
2019-03-06 01:10:03 +0100

    Improved deserialization for some types

--- bot/utilities/sc2_serialization.h
+++ bot/utilities/sc2_serialization.h
@@ -68,6 +68,7 @@ namespace sc2 {
             cereal::make_nvp("target_pos", unit.target_pos),
             cereal::make_nvp("progress", unit.progress)
         );
+        unit.ability_id = ability_id;
     }
 
     template <class Archive>
@@ -83,10 +84,11 @@ namespace sc2 {
             cereal::make_nvp("energy_max", unit.energy_max),
             cereal::make_nvp("unit_type", unit_type)
         );
+        unit.unit_type = unit_type;
     }
 
     template <class Archive>
-    void serialize(Archive& archive, Unit& unit) {
+    void save(Archive& archive, const Unit& unit) {
         UNIT_TYPEID unit_type = unit.unit_type;
         std::vector<BUFF_ID> buffs;
         for (auto b : unit.buffs) buffs.push_back(b);
@@ -124,5 +126,49 @@ namespace sc2 {
             cereal::make_nvp("is_alive", unit.is_alive),
             cereal::make_nvp("last_seen_game_loop", unit.last_seen_game_loop)
         );
+    }
+
+    template <class Archive>
+    void load(Archive& archive, Unit& unit) {
+        UNIT_TYPEID unit_type = unit.unit_type;
+        std::vector<BUFF_ID> buffs;
+        archive(
+            cereal::make_nvp("display_type", unit.display_type),
+            cereal::make_nvp("tag", unit.tag),
+            cereal::make_nvp("unit_type", unit_type),
+            cereal::make_nvp("owner", unit.owner),
+            cereal::make_nvp("pos", unit.pos),
+            cereal::make_nvp("facing", unit.facing),
+            cereal::make_nvp("radius", unit.radius),
+            cereal::make_nvp("build_progress", unit.build_progress),
+            cereal::make_nvp("cloak", unit.cloak),
+            cereal::make_nvp("detect_range", unit.detect_range),
+            cereal::make_nvp("is_blip", unit.is_blip),
+            cereal::make_nvp("health", unit.health),
+            cereal::make_nvp("health_max", unit.health_max),
+            cereal::make_nvp("shield", unit.shield),
+            cereal::make_nvp("shield_max", unit.shield_max),
+            cereal::make_nvp("energy", unit.energy),
+            cereal::make_nvp("energy_max", unit.energy_max),
+            cereal::make_nvp("mineral_contents", unit.mineral_contents),
+            cereal::make_nvp("vespene_contents", unit.vespene_contents),
+            cereal::make_nvp("is_flying", unit.is_flying),
+            cereal::make_nvp("is_burrowed", unit.is_burrowed),
+            cereal::make_nvp("weapon_cooldown", unit.weapon_cooldown),
+            cereal::make_nvp("orders", unit.orders),
+            cereal::make_nvp("add_on_tag", unit.add_on_tag),
+            cereal::make_nvp("passengers", unit.passengers),
+            cereal::make_nvp("cargo_space_taken", unit.cargo_space_taken),
+            cereal::make_nvp("cargo_space_max", unit.cargo_space_max),
+            cereal::make_nvp("engaged_target_tag", unit.engaged_target_tag),
+            cereal::make_nvp("buffs", buffs),
+            cereal::make_nvp("is_powered", unit.is_powered),
+            cereal::make_nvp("is_alive", unit.is_alive),
+            cereal::make_nvp("last_seen_game_loop", unit.last_seen_game_loop)
+        );
+
+        unit.unit_type = unit_type;
+        unit.buffs.clear();
+        for (auto b : buffs) unit.buffs.push_back(b);
     }
 };
