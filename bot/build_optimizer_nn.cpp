a3917cb3b59b6611b8656461c2c0bdc8ee66ef17
Aron Granberg <aron.granberg@gmail.com>
2019-04-03 20:39:26 +0200

    Add DISABLE_PYTHON support to build time predictor

--- bot/build_optimizer_nn.cpp
+++ bot/build_optimizer_nn.cpp
@@ -1,25 +1,32 @@
 #include "build_optimizer_nn.h"
 #include <iostream>
 #include <pybind11/stl.h>
+#include <pybind11/embed.h>
 #include "utilities/python_utils.h"
 
 using namespace std;
 using namespace sc2;
 
 void BuildOptimizerNN::init() {
-    // pybind11::exec(R"(
-    //     import sys
-    //     sys.path.append("bot/optimizer_train")
-    // )");
+#if !DISABLE_PYTHON
+    pybind11::exec(R"(
+        import sys
+        sys.path.append("bot/optimizer_train")
+    )");
     pybind11::module trainer = pybind11::module::import("optimizer_train");
     predictFunction = trainer.attr("predict");
     cout << "Finished initializing NN" << endl;
+#endif
 }
 
 vector<float> BuildOptimizerNN::predictTimeToBuild(const vector<pair<int, int>>& startingState, const BuildResources& startingResources, const vector < vector<pair<int, int>>>& targets) const {
+#if !DISABLE_PYTHON
     lock_guard<mutex> lock(python_thread_mutex);
 
     auto res = predictFunction(startingState, tuple<int,int>(startingResources.minerals, startingResources.vespene), targets);
     return res.cast<std::vector<float>>();
     // return vector<float>();
+#else
+    return vector<float>(targets.size());
+#endif
 }
