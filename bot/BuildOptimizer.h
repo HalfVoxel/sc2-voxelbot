614f202aa6b34007489e1b512d1b0b75d8ac936e
Aron Granberg <aron.granberg@gmail.com>
2019-05-03 14:28:54 +0200

    Remove old build order optimizer

--- bot/BuildOptimizer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "sc2api/sc2_interfaces.h"
-
-struct BuildOptimizer {
-    void init();
-    std::vector<sc2::UNIT_TYPEID> calculate_build_order(sc2::Race race, const std::vector<std::pair<sc2::UNIT_TYPEID, int>>& start, const std::vector<std::pair<sc2::UNIT_TYPEID, int>>& target);
-};
-
-void unitTestBuildOptimizer(BuildOptimizer& optimizer);
